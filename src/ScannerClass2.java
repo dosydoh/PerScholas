b224d2154faca2643c38d32bd5d6be055b627e46
dosydoh <dos.fancy@gmail.com>
2024-05-27 20:49:22 -0700

    Scanner Class Assignment

--- src/ScannerClass2.java
+++ src/ScannerClass2.java
@@ -1,14 +1,16 @@
 import java.util.Scanner;
 
-public class Solution {
+public class ScannerClass2 {
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int i = scan.nextInt();
-        double j = scan.nextInt();
 
         // Write your code here.
-
+        double d = scan.nextDouble();
+        scan.nextLine();
+        String s = scan.nextLine();
+        scan.close();
         System.out.println("String: " + s);
         System.out.println("Double: " + d);
         System.out.println("Int: " + i);
